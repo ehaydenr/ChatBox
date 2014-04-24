f4bd4489035086bda35380d67ad4aefa81410524
Eric Rosenberg <ehaydenr@gmail.com>
2014-04-23 21:31:18 -0500

    added a ton of comments and setup remove socket in sockets list upon disconnect

--- chat.js
+++ chat.js
@@ -1,34 +1,73 @@
+// Grab all dependencies
 var express = require('express'),
 	app		= express(),
 	http	= require('http'),
-	server	= http.createServer(app),
-	path	= require('path'),
-	io		= require('socket.io').listen(server);
+	path	= require('path');
 
+// Create server
+var server	= http.createServer(app);
+
+// Socket Map 
+// Key: 	socket.id 
+// Value: 	socket
 var sockets = {};
 
-server.listen(3000);
-
+// Configure Express
 app.configure(function (){
+
+	// Tell express where the views are
 	app.set('views', 'views/');
+
+	// Tell express what view engine we are going to use
 	app.set('view engine', 'jade');
+
+	// Give express the path to the public files (javascripts and css files to be given to client)
 	app.use(express.static(path.resolve('./public')));
 });
 
+// Define the root path of the website. This is called when http://mydomain.com/ is called
 app.get('/', function (req, res) {
-	res.render('index');
+
+	// res is the response variable. Tell it to render the index supplied in the views folder. It knows to check the views folder because of the Express configuration we defined above.
+    res.render('index');
 });
 
+// Startup the server and let the requests roll in
+server.listen(3000);
+
+// Listen for websocket interactions
+var io = require('socket.io').listen(server);
+
+//Startup websocket listeners and callbacks with socket.io
+
+// Listen for a connection made with client. Trigger callback when made
 io.sockets.on('connection', function (socket) {
 
+	console.log("New websocket connection: " + socket.id);
+
+	// Connection made, add the socket to the map of socket ids to sockets
 	sockets[socket.id] = socket;
 
+	// Listen for incoming messages with the title 'relay_me'
 	socket.on('relay_me', function (message) {
+
+		// TODO: Filter or verify message.
+
+		// Relay me has been triggered. Go ahead and send the message out to everyone
 		for(s in sockets){
 			if(sockets[s])
-				sockets[s].emit("incoming_message", message);
-			else console.log("Broken Socket");
+				sockets[s].emit("incoming_message", message);	// Send out message
+			else console.log("Broken Socket");	// For some reason the socket.id was in our map but the socket was broken.
 		}
 	});
 
+	// Connection with client disconnected listener
+    socket.on('disconnect', function(){
+        console.log('Removing: ' + socket.id);
+
+        // Delete socket id from map. We won't interact with them anymore.
+        delete sockets[socket.id];
+    });
+
 });
+
--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,25 +1,40 @@
+// Startup websocket connection
 var socket = io.connect('/'),
 	username = "";
 
 $(document).ready(function (){
+
+	// Get username from client
 	username = prompt("Please enter your name");
 
+	// Define callback for when a message is sent over websockets
 	socket.on('incoming_message', function (message) {
-    $('.output').append($.parseHTML('<strong>'+message.username+'</strong>' + ": " + message.content + "<br />"));
+
+		// Append the output window with the message. Prevent javascript injection
+    	$('.output').append($.parseHTML('<strong>'+message.username+'</strong>' + ": " + message.content + "<br />"));
+
+    	// Scroll the div to the most recently appended message
 		$('.output').scrollTop($('.output')[0].scrollHeight);
 	});
 
+	// Let everyone know you joined the chat server. It is client side and easily removable but it is not a security risk.
 	sendMessage("Hi! I just joined");
 
+	// Get input text when the user hits the enter key
 	$('.input').on('keypress', function (event) {
 		if(event.keyCode == 13){
 			event.preventDefault();
+
+			// Send message containing the value of the input box
 			sendMessage($(this).val());
+
+			// Clear the value of the input box
 			$(this).val('');
 		}
 	});
 });
 
+// Send message to the server with my username and message. Username forgery is easy but lets not worry about that for now. We can handle with sessions later
 function sendMessage(content){
 	socket.emit('relay_me', {username:username, content:content});
 }
